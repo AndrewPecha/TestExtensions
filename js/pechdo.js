5aa4a1610c500ecad2e07537c47d13a80fbecdd2
AndrewPecha <andrew.pecha@gmail.com>
2019-11-03 23:27:32 -0600

    created basic createWindow method to create a movable modal with static content

---
+++ js/pechdo.js
@@ -0,0 +1,66 @@
+function createWindow(options) {
+    if(options.modalId === undefined)
+        return;
+
+    var settings = $.extend({
+        modalContent: ""
+    }, options);
+
+    $('#' + settings.modalId).addClass('modal').html(
+            "<div id=\'" + settings.modalId + "Header\' class=\'modalHeader\' onmousedown=\'moveElement(event, this)\'>" +
+                "<div class='modalCloseButton' onmousedown='event.stopPropagation()' onclick='toggleElement(event, findClassFromChild(this, \'modal\'))'>X</div>" +
+            "</div>" +
+            "<div id=testModalContent>" +
+                settings.modalContent +
+            "</div>"
+    );
+}
+
+function loadRemoteModalContent(url, modalId){
+    $.ajax({
+        url: url,
+        success: function(data){
+            $('#'+ modalId + "Content").html(data);
+        }
+    })
+}
+
+function toggleElement(e, element){                
+    e.stopPropagation();
+    if(element === undefined)
+        return;
+
+    $(element).toggle();
+}
+
+function moveElement(e, element){
+    e.stopPropagation();
+    if(element.classList.contains("modalHeader"))
+        element = element.parentElement;
+    $(element).data('offSetX', e.offsetX);
+    $(element).data('offSetY', e.offsetY);
+    
+    document.onmousemove = function(f){
+        setElementToEventCoords(f, element);
+    }      
+
+    document.onmouseup = function(e){
+        document.onmousemove = null;
+    }                
+}       
+
+function setElementToEventCoords(e, element){
+        var offSetX = $(element).data('offSetX');
+        var offSetY = $(element).data('offSetY');
+        $(element).css({top: e.pageY - offSetY, left: e.pageX - offSetX});
+}
+
+function findClassFromChild(element, classToFind){
+    if(element.classList.contains(classToFind))
+        return element;
+
+    if(element.parentElement === undefined)
+        return undefined;
+
+    return findClassFromChild(element.parentElement, classToFind);
+}
